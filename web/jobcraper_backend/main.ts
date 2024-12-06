6530685ec54fe82462a1fe7ad8e304a4f13e868f
NxtPerfect <alakaxan@protonmail.com>
2024-12-06 09:31:29 +0100

    Preparing statistics page

--- web/jobcraper_backend/main.ts
+++ web/jobcraper_backend/main.ts
@@ -7,7 +7,7 @@ const db = new Database("../../db/database.db");
 
 app.get('/offers', (c) => {
   try {
-    const stmt = db.prepare("SELECT * FROM offers ORDER BY last_seen;");
+    const stmt = db.prepare("SELECT * FROM offers ORDER BY last_seen DESC;");
     const offers = stmt.all() as Array<{
       id: string,
       title: string,
@@ -22,13 +22,12 @@ app.get('/offers', (c) => {
     return c.json(offers);
   } catch (error) {
     console.error("Error fetching offers:", error);
-    return c.json({ error: `Failed to fetch offers ${error}` }, 500);
+    return c.json({ error: "Failed to fetch offers" }, 500);
   }
 });
 
 app.post('/update/offers/is_applied', async (c) => {
   const data = await c.req.json();
-  console.log(data.id);
   try {
     const stmt = db.prepare("UPDATE offers SET is_applied=? WHERE id=?;");
     const hashId = data.id;
@@ -41,6 +40,31 @@ app.post('/update/offers/is_applied', async (c) => {
     return c.json({ error: `Failed to update offers ${error}` }, 500);
   }
 });
+
+app.get('/statistics', (c) => {
+  try {
+    const stmt = db.prepare(`SELECT
+  technologies,
+  COUNT(DISTINCT technologies) as count_technologies,
+  is_applied,
+  COUNT(is_applied) as count_is_applied,
+  by_company,
+  COUNT(DISTINCT by_company) as count_by_company
+FROM offers;`);
+    const statistics = stmt.all() as Array<{
+      technologies: Array<string>,
+      count_technologies: number,
+      is_applied: Array<string>,
+      count_is_applied: number,
+      by_company: string,
+      count_by_company: number
+    }>;
+    return c.json(statistics);
+  } catch (error) {
+    console.error("Failed to fetch statistics:", error);
+    return c.json({ error: "Failed to fetch statistics" }, 500);
+  }
+})
 
 app.onError((err, c) => {
   return c.text("Unexpected error: ", err);
